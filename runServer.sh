9c11f22c36279fa3f7d47836f2c746d88edd8bdc
y3tu <14643092@qq.com>
2020-08-02 09:56:26 +0800

    update

--- runServer.sh
+++ runServer.sh
@@ -1,4 +1,5 @@
 docker stop $(docker ps -a | grep "yao-ui" | awk '{print $1 }')
 docker rm $(docker ps -a | grep "yao-ui" | awk '{print $1 }')
 docker rmi $(docker images | grep "yao-ui" | awk '{print $3}')
-
+docker pull y3tu-docker.pkg.coding.net/yao/yao/yao_yao-ui:latest
+docker run -d -p 80:80 --name yao-ui y3tu-docker.pkg.coding.net/yao/yao/yao_yao-ui:latest
