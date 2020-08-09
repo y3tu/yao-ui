5b1b828665c8c262f6bf91e34a36a21eca2ac7a3
y3tu <14643092@qq.com>
2020-08-09 21:06:29 +0800

    update

---
+++ src/components/DynamicForm/Index.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="container">
+        <div class="left-board">
+            <el-scrollbar class="left-scrollbar">
+                <el-tabs v-model="activeTabName" :stretch="true">
+                    <el-tab-pane label="常用组件" name="common">
+                        <div class="components-list">
+                            <draggable
+                                    class="components-draggable"
+                                    :list="commonComponents"
+                                    :group="{ name: 'componentsGroup', pull: 'clone', put: false }"
+                                    :clone="cloneComponent"
+                                    draggable=".components-item"
+                                    :sort="false"
+                                    @end="onEnd"/>
+                        </div>
+                    </el-tab-pane>
+                </el-tabs>
+            </el-scrollbar>
+        </div>
+    </div>
+</template>
+
+<script>
+    import draggable from "vuedraggable";
+    //获取组件
+    import {
+        formConf,
+        inputComponents,
+        selectComponents,
+        layoutComponents,
+        commonComponents,
+        customMadeComponents,
+    } from "./components/generator/config";
+
+    export default {
+        components: {
+            draggable
+        },
+        data(){
+            return{
+                formConf,
+                inputComponents,
+                selectComponents,
+                layoutComponents,
+                commonComponents,
+                customMadeComponents,
+                activeTabName: "common",
+            }
+        }
+    }
+</script>
--- src/router/extraRouter.js
+++ src/router/extraRouter.js
@@ -5,6 +5,12 @@ const extraRouter=[
         component: () => import('@/views/model/support/datav/Designer'),
         hidden: true
     },
+    {
+        name: 'dynamicForm',
+        path: '/dynamicForm',
+        component: () => import('@/components/DynamicForm/Index'),
+        hidden: true
+    },
 ];
 
 export default extraRouter
